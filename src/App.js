a230b05c3b67f6e8a3f65a3682733740dba0bd5e
Hamza <hamzawol2018@gmail.com>
2025-11-16 21:31:14 +0100

    h1 changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hamza is here</h1>
+        <h1>Hamza is not here</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
